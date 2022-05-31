ce0a176389dbcee5ff353d3926e5bc623efbe761
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-05-31 12:06:10 -0400

    TDD no cambia monedas por otras monedas refactorizado

--- src/cambiomonedas.js
+++ src/cambiomonedas.js
@@ -1,5 +1,9 @@
 function cambiar(a) {
-    return a;
+    let cambio;
+    if (a==0.1 || a==0.2 || a==0.5 || a==1 ||a==2 ||a==5) {
+        cambio=a;
+    }
+    return cambio;
   }
   
 export default cambiar;
