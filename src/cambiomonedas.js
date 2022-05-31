a0b002de01fee4107267ed3dd1a133c6f9c52bd0
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-05-31 12:10:53 -0400

    TDD se devuelve cambio de puras monedas de 1 bs test aprobado sin refactor

--- src/cambiomonedas.js
+++ src/cambiomonedas.js
@@ -3,6 +3,9 @@ function cambiar(a) {
     if (a==0.1 || a==0.2 || a==0.5 || a==1 ||a==2 ||a==5) {
         cambio=a;
     }
+    else{
+        cambio = "1,1,1,1,1,1";
+    }
     return cambio;
   }
   
