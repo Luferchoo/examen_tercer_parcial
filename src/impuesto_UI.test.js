17ab905ad71a57e9a4282ac38f1c2f7ffa020de7
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-05-31 11:49:11 -0400

    segunda prueba falla

--- src/impuesto_UI.test.js
+++ src/impuesto_UI.test.js
@@ -17,6 +17,17 @@ describe("devolver cambio", () => {
     */
     expect(monto1.value).toEqual("200");
   });
+  it("Si ingresa efectivo 200 se guarda efectivo como 200", () => {
+    const monto1 = document.querySelector("#efectivo");
+    efectivo1.value = "200";
+/*
+    const calcular = document.querySelector("#boton-calcular");
+    calcular.click();
+
+    const resultado = document.querySelector("#resultado-div");
+    */
+    expect(efectivo1.value).toEqual("200");
+  });
 });
 
   
