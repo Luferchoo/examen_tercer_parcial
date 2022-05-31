28797f0f721407255282ec5abb552ffe204ef80d
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-05-31 12:02:38 -0400

    TDD no cambia monedas por otras monedas test aprobado

--- src/cambiomonedas.test.js
+++ src/cambiomonedas.test.js
@@ -4,4 +4,7 @@ describe("cambiar", () => {
   it("deberia cambiar monedas simples en su unidad minima", () => {
     expect(cambiar(1)).toEqual(1);
   });
+  it("no cambia monedas por otras monedas", () => {
+    expect(cambiar(5)).toEqual(5);
+  });
 });
--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,7 @@
 import sumar from "./sumador";
 
 const monto1 = document.querySelector("#monto");
-const second = document.querySelector("#efectivo");
+const efectivo1 = document.querySelector("#efectivo");
 const form = document.querySelector("#cambio-form");
 const div = document.querySelector("#resultado-div");
 
@@ -9,7 +9,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(monto1.value);
-  const secondNumber = Number.parseInt(second.value);
+  const secondNumber = Number.parseInt(efectivo1.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
